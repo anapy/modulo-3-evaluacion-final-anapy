784f3ad151ea8897a29af715bd4be00c9276e550
anapy <anabelenfunesc@gmail.com>
2020-08-06 18:04:06 +0200

    adding more style changes

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -5,7 +5,7 @@ import '../stylesheets/characterCard.scss';
 const CharacterCard = props => {
   return (
     <article className="character__container"> 
-      <img src={props.imgURL} className="character__img" alt={props.name} width="180px"/>
+      <img src={props.imgURL} className="character__img" alt={props.name} width="174px"/>
       <h3 className="character__name">{props.name}</h3>
       <p className="character__species">{props.species} </p>
     </article>
