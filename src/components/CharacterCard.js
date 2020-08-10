fa7d9d384bed4fa5fc114c3fefeeb629b2830195
anapy <anabelenfunesc@gmail.com>
2020-08-10 10:26:02 +0200

    destructuring in CharacterCard

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -4,18 +4,19 @@ import PropTypes from 'prop-types';
 import '../stylesheets/characterCard.scss';
 
 const CharacterCard = props => {
-  const renderDetailsClick = (ev) => {
+  const { id, imgURL, name, species, renderDetailsClick } = props;
+  const renderDetailsClickCard = (ev) => {
     const id = ev.currentTarget.id;
-    props.renderDetailsClick(id);
+    renderDetailsClick(id);
   }
 
   return (
-    <article className="character__card__container" id={props.id} onClick={renderDetailsClick}>
-      <Link to={`/details/${props.name.toLowerCase().replace(' ', '')}`} className="character__card__link">
-        <img src={props.imgURL} className="character__card__img" alt={props.name} width="174px"/>
+    <article className="character__card__container" id={id} onClick={renderDetailsClickCard}>
+      <Link to={`/details/${name.toLowerCase().replace(' ', '')}`} className="character__card__link">
+        <img src={imgURL} className="character__card__img" alt={name} width="174px"/>
       </Link>
-      <h3 className="character__card__name">{props.name}</h3>
-      <p className="character__card__species">{props.species} </p>
+      <h3 className="character__card__name">{name}</h3>
+      <p className="character__card__species">{species} </p>
     </article>
   );
 }
