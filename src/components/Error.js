89a4aa48eba5b3fc9ba7d677db63b6fe3e5eac1a
anapy <anabelenfunesc@gmail.com>
2020-08-10 10:26:59 +0200

    destructuring in erro

--- src/components/Error.js
+++ src/components/Error.js
@@ -4,16 +4,17 @@ import PropTypes from 'prop-types';
 import '../stylesheets/error.scss';
 
 const Error = props => {
+  const{ searchCharacter} = props;
   return (
     <div>
       <h4 className="errorText">
         There is no character matching your search
-        <span className="errorText__word"> {props.searchCharacter}</span>
+        <span className="errorText__word"> {searchCharacter}</span>
       </h4>
       <div>
         <img src="https://media1.tenor.com/images/e620a0fbd908b1427448972e843f8b00/tenor.gif?itemid=9603354" alt="sad_gif"></img>
       </div>
-      <div className={`return_btn_container ${props.searchCharacter !== '' ? 'hidden' : ''}`}>
+      <div className={`return_btn_container ${searchCharacter !== '' ? 'hidden' : ''}`}>
         <Link to ="/" className="return_btn">
           <span >Return</span>
         </Link>
